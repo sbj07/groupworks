c51b9ffdb3216189544684fd456919053a29a29d
jiseop <jiseop992@gmail.com>
2024-01-08 20:37:08 +0900

    [백엔드]

--- workspace/groupworks/src/main/java/com/groupworks/app/member/api/contoller/MemberController.java
+++ workspace/groupworks/src/main/java/com/groupworks/app/member/api/contoller/MemberController.java
@@ -1,10 +1,138 @@
 package com.groupworks.app.member.api.contoller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.groupworks.app.member.service.MemberService;
+import com.groupworks.app.member.vo.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/member")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService service;
+
+    // 회원가입
+    @PostMapping("signup")
+    public Map<String, String> signup(MemberVo vo){
+        int result = service.signup(vo);
+        Map<String, String> map = new HashMap<String,String>();
+        map.put("msg","okay");
+        if(result != 1) {
+            map.put("msg", "nope");
+        }
+        return map;
+    }
+
+    // 로그인
+    @PostMapping
+    public Map<String, Object> login(MemberVo vo) throws Exception {
+        MemberVo loginMember = service.login(vo);
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "okay");
+        map.put("loginMember", loginMember);
+        if(loginMember == null){
+            map.put("msg", "nope");
+        }
+        return map;
+    }
+    // 회원탈퇴
+    @DeleteMapping
+    public Map<String, String> deleteMember(String no){
+        int result = service.deleteMember(no);
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("msg","okay");
+        if(result != 1){
+            map.put("msg", "nope");
+        }
+        return map;
+    }
+
+    // ---------------- 목록조회 ------------------
+
+    // 권한
+    @GetMapping("auth")
+    public Map<String, Object> getAuthList(){
+        List<AuthVo> authList = service.getAuthList();
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "okay");
+        map.put("authList", authList);
+        if(authList == null) {
+            map.put("msg", "nope");
+        }
+        return map;
+    }
+
+    // 부서
+    @GetMapping("depart")
+    public Map<String, Object> getDepartList(){
+        List<DepartVo> departList = service.getDepartList();
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "okay");
+        map.put("authList", departList);
+        if(departList == null) {
+            map.put("msg", "nope");
+        }
+        return map;
+    }
+    // 회사
+    @GetMapping("company")
+    public Map<String, Object> getCompanyList(){
+        List<CompanyVo> companyList = service.getCompanyList();
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "okay");
+        map.put("authList", companyList);
+        if(companyList == null) {
+            map.put("msg", "nope");
+        }
+        return map;
+    }
+
+    // 직책
+    @GetMapping("position")
+    public Map<String, Object> getPositionList(){
+        List<PositionVo> positionList = service.getPositionList();
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "okay");
+        map.put("authList", positionList);
+        if(positionList == null) {
+            map.put("msg", "nope");
+        }
+        return map;
+    }
+
+    // 근무싱태
+    @GetMapping("workStat")
+    public Map<String, Object> getWorkStatusList(){
+        List<WorkStatusVo> workStatList = service.getWorkStatList();
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "okay");
+        map.put("authList", workStatList);
+        if(workStatList == null) {
+            map.put("msg", "nope");
+        }
+        return map;
+    }
+    // ----------------- 변경 -------------------
+
+    // 근무상태
+    @PutMapping("workStat")
+    public Map<String, String> editWorkStat(MemberVo vo){
+        int result = service.editWorkStatus(vo);
+        Map<String, String > map = new HashMap<String, String>();
+        map.put("msg", "okay");
+
+        if(result != 1) {
+            msp
+        }
+    }
+    // 권한
+
+    // 연차
+
+    // 회사추가
 
 }
--- workspace/groupworks/src/main/java/com/groupworks/app/member/dao/MemberDao.java
+++ workspace/groupworks/src/main/java/com/groupworks/app/member/dao/MemberDao.java
@@ -1,4 +1,67 @@
 package com.groupworks.app.member.dao;
 
+import com.groupworks.app.member.vo.*;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class MemberDao {
+
+    // 화원가입
+    public int signup(SqlSessionTemplate sessionTemplate, MemberVo vo){
+        return sessionTemplate.insert("MemberMapper.signup",vo);
+    }
+
+    // 로그인
+    public MemberVo login(SqlSessionTemplate sessionTemplate, MemberVo vo){
+        return sessionTemplate.selectOne("MemberMapper.login", vo);
+    }
+
+    // 회원탈퇴
+    public int deleteMember(SqlSessionTemplate sessionTemplate, String no){
+        return sessionTemplate.update("MemberMapper.deleteMember", no);
+    }
+
+    // 권한 목록 조회
+    public List<AuthVo> getAuthList(SqlSessionTemplate sessionTemplate){
+        return sessionTemplate.selectList("MemberMapper.getAuthList");
+    }
+
+    // 부서 목록 조회
+    public List<DepartVo> getDepartList(SqlSessionTemplate sessionTemplate){
+        return sessionTemplate.selectList("MemberMapper.getDepartList");
+    }
+
+    // 회사 목록 조회
+    public List<CompanyVo> getCompanyList(SqlSessionTemplate sessionTemplate){
+        return sessionTemplate.selectList("MemberMapper.getCompanyList");
+    }
+
+    // 직책 목록 조회
+    public List<PositionVo> getPositionList(SqlSessionTemplate sessionTemplate){
+        return sessionTemplate.selectList("MemberMapper.getPositionList");
+    }
+
+    // 근무 상태 목록 조회
+    public List<WorkStatusVo> getWorkStatList(SqlSessionTemplate sessionTemplate){
+        return sessionTemplate.selectList("MemberMapper.getWorkStatList");
+    }
+
+    // 권한 변경
+    public int editAuth(SqlSessionTemplate sessionTemplate, MemberVo vo){
+        return sessionTemplate.update("MemberMapper.editAuth", vo);
+    }
+
+    // 근무 상태 변경
+    public int editWorkStatus(SqlSessionTemplate sessionTemplate, MemberVo vo){
+        return sessionTemplate.update("MemberMapper.editWorkStatus", vo);
+    }
+
+    // 회사 추가
+    public int insertCompany(SqlSessionTemplate sessionTemplate, CompanyVo vo){
+        return sessionTemplate.insert("MemberMapper.insertCompany", vo);
+    }
+
 }
