98eb5993c52144fb79e6b764619a8fdb66ec8b4a
DESKTOP-PN6OK2K\sung <ronaldo39@naver.com>
2024-01-09 17:43:19 +0900

    [todo] todo controller 작성

--- workspace/groupworks/src/main/java/com/groupworks/app/payform/service/PayFormService.java
+++ workspace/groupworks/src/main/java/com/groupworks/app/payform/service/PayFormService.java
@@ -1,10 +1,54 @@
 package com.groupworks.app.payform.service;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
+import com.groupworks.app.payform.dao.PayFormDao;
+import com.groupworks.app.payform.vo.PayFormVo;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class PayFormService {
-public void write() {
-		
+
+	
+	private final PayFormDao dao;
+	private final SqlSessionTemplate sst;
+	
+	//지출결의서 전체조회
+	public List<PayFormVo> list() {
+		return dao.list(sst);
+	}
+	
+	//결재대기 목록 조회
+	public List<PayFormVo> ingApprove() {
+		return dao.ingApprove(sst);
+	}
+	
+	//결재완료 목록 조회
+	public List<PayFormVo> edApproved() {
+		return dao.edApprove(sst);
+	}
+	
+	//지출결의서 작성
+	public int write(PayFormVo vo) {
+		return dao.write(sst, vo);
+	}
+	
+	//지출결의서 승인
+	public int apply(PayFormVo vo) {
+		return dao.apply(sst, vo);
+	}
+	
+	//지출결의서 반려
+	public int rejection(PayFormVo vo) {
+		return dao.rejection(sst, vo);
+	}
+	
+	//지출결의서 삭제
+	public int delete(String no) {
+		return dao.delete(sst, no);
 	}
 }
--- workspace/groupworks/src/main/java/com/groupworks/app/todo/controller/TodoController.java
+++ workspace/groupworks/src/main/java/com/groupworks/app/todo/controller/TodoController.java
@@ -1,10 +1,75 @@
 package com.groupworks.app.todo.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-@Controller
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.groupworks.app.todo.service.TodoService;
+import com.groupworks.app.todo.vo.TodoVo;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/todo")
+@RequiredArgsConstructor
 public class TodoController {
-public void write() {
-		
+
+	private final TodoService service;
+	
+	//할일 조회
+	@GetMapping("list")
+	public Map<String, Object> list() {
+		List<TodoVo> todoList = service.list();
+		Map<String, Object> map = new HashMap<>();
+		map.put("msg", "good");
+		map.put("todoList", todoList);
+		if(todoList == null) {
+			map.put("msg", "bad");
+		}
+		return map;
+	}
+	
+	//할일 작성
+	@PostMapping("write")
+	public Map<String, String> write(TodoVo vo){
+		int result = service.write(vo);
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("msg", "good");
+		if(result != 1) {
+			map.put("msg", "bad");
+		}
+		System.out.println(result);
+		return map;
+	}
+	
+	//할일 수정
+	@PostMapping("edit")
+	public Map<String, String> edit(TodoVo vo) {
+		int result = service.edit(vo);
+		Map<String, String> map = new HashMap<>();
+		map.put("msg", "good");
+		if(result != 1) {
+			map.put("msg", "bad");
+		}
+		return map;
+	}
+	
+	//할일 삭제
+	@DeleteMapping
+	public Map<String, String> delete(String no){
+		int result = service.delete(no);
+		Map<String, String> map = new HashMap<>();
+		map.put("msg", "good");
+		if(result != 1) {
+			map.put("msg", "bad");
+		}
+		return map;
 	}
 }
