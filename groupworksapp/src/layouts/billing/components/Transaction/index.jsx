17736c8ed886adbac73ed839bb7ed4306ec64383
DESKTOP-PN6OK2K\sung <ronaldo39@naver.com>
2024-01-14 23:59:09 +0900

    [할일작성] 완료

--- groupworksapp/src/layouts/billing/components/Transaction/index.jsx
+++ groupworksapp/src/layouts/billing/components/Transaction/index.jsx
@@ -14,6 +14,28 @@ function Transaction({ icon, name, description, value, onDelete }) {
     setColor(color === "secondary" ? "success" : "secondary");
   };
 
+  const [inputValue, setInputValue] = useState('');
+
+  const handleRegister = () => {
+    console.log('memberNo:', 2);
+    console.log('content:', inputValue);
+    
+    fetch('http://127.0.0.1:8888/app/api/todo/write', { // 여기에 요청하려는 API 주소를 넣으세요.
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ content: inputValue, memberNo:2}),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  };
+  
   return (
     <MDBox component="div" py={1} pr={2} mb={1} onClick={handleClick}>
       <MDBox display="flex" justifyContent="space-between" alignItems="center">
@@ -26,7 +48,8 @@ function Transaction({ icon, name, description, value, onDelete }) {
 
           <MDBox display="flex" flexDirection="column" >
               <MDBox>
-                <MDInput type="text" variant="standard" name="pwd" placeholder='새로운 할일' fullWidth style={{ width: '200%' }}/>
+                <MDInput type="text" variant="standard" name="pwd" placeholder='새로운 할일' fullWidth style={{ width: '200%' }}
+                value={inputValue} onChange={e => setInputValue(e.target.value)}/>
               </MDBox>
             <MDTypography variant="caption" color="text" fontWeight="regular">
               {description}
@@ -36,7 +59,7 @@ function Transaction({ icon, name, description, value, onDelete }) {
         <MDTypography variant="button" color={color} fontWeight="medium" textGradient>
           {value}
         </MDTypography>
-          <MDButton style={{marginLeft:'39%'}}variant="gradient" color="success" size="small" type="submit">
+          <MDButton style={{marginLeft:'39%'}}variant="gradient" color="success" size="small" type="submit" onClick={handleRegister}>
             등록
           </MDButton>
           <MDButton variant="gradient" color="error" size="small" type="button" onClick={onDelete}>
