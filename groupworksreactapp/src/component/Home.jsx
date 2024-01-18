128d8e5d24ef2f72a5ddff05921bc967efab5000
jiseop992 <jiseop992@gmail.com>
2024-01-18 20:21:37 +0900

    [레이아웃 수정]

---
+++ groupworksreactapp/src/component/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import TodoList from './todo/TodoList';
+
+
+const Home = () => {
+    return (
+        <>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <TodoList/>
+        </>
+    );
+};
+
+export default Home;
--- groupworksreactapp/src/component/Sidebar/Sidebar.jsx
+++ groupworksreactapp/src/component/Sidebar/Sidebar.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import { NavLink, Route, Routes, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components'
-import SidebarItem from './SidebarItem';
 
 const StyledSideDiv = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
-
+    justify-content: space-around;
     margin-top: 10%;
     margin-left: 10%;
     border-radius: 10px;
     width: 100%;
     background-color: #282c34;
     height: 100%;
-    
+    & > div,StyledLogoutDiv {
+        display: flex;
+        flex-direction: column;
+        font-size: 2rem;
+        color: white;
+    }
 `;
 
 const StyledMenuDiv = styled.div`
     width: 80%;
     display: flex;
     flex-direction: column;
+    font-size: 2rem;
+    color: white;
 `;
 
 const StyledLogoutDiv = styled.div`
@@ -29,41 +34,25 @@ const StyledLogoutDiv = styled.div`
     bottom: 15px;
     height: 10%;
     width: 10%;
+    margin-left: -5%;
     background-color: #61dafb;
     font-size: large;
     text-align: center;
+    color: white;
 `;
 
 
 const Sidebar = () => {
-    const menus = [
-        { name: "Home", path: "/" } ,
-        { name: "공지사항", path: "/notice" } ,
-        { name: "전자결재", path: "/document" } ,
-        { name: "조직도", path: "/organ" } ,
-    ]
     return (
         <StyledSideDiv>
-            <StyledMenuDiv>
-                {
-                    menus.map( (menu, index) => {
-                        return (
-                            <NavLink
-                                style={ {color:"gray"} }
-                                to={menu.path}
-                                key={index}
-                            >
-                                <SidebarItem
-                                    menu={menu}
-                                />
-                            </NavLink>
-                        )
-                    })
-                }
-                <StyledLogoutDiv>
-                    LOGOUT
-                </StyledLogoutDiv>
-            </StyledMenuDiv>
+                    <div><Link to="/">Home</Link></div>
+                    <div><Link to="/notice/list">공지사항</Link></div>
+                    <div><Link to="/document/list">전자결재</Link></div>
+                    <div><Link to="/organ/list">조직도</Link></div>
+                    <div><Link to="/organ/list">예약</Link></div>
+                    <div>
+                        <StyledLogoutDiv>LOGOUT</StyledLogoutDiv>
+                    </div>
         </StyledSideDiv>
     );
 };
