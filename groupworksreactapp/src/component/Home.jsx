83eddfaca218c725133bc7d63c6cea3462ae6dce
jiseop992 <jiseop992@gmail.com>
2024-01-19 09:16:21 +0900

    [메인페이지 변경]

--- groupworksreactapp/src/component/Home.jsx
+++ groupworksreactapp/src/component/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import TodoList from './todo/TodoList';
-
+import NoticeList from './notice/NoticeList';
 
 const Home = () => {
     return (
         <>
             <div>1</div>
             <div>2</div>
-            <div>3</div>
-            <TodoList/>
+            <NoticeList />
+            <TodoList />
         </>
     );
 };
